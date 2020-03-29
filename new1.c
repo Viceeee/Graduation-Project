5504b7b71906d31347ce0b04c84da8d3109e3938
Viceeee <378295363@qq.com>
2020-03-29 19:00:58 +0800

    3.29commit

--- new1.c
+++ new1.c
@@ -19,32 +19,51 @@ int main ()
    return 0;
 }
 */
-
+#define FREE 0  //定义FREE为0
+#define DIRLEN 80//定义dirlen的长度为80
+#define END 65535 //定义结束盘号为65535
+#define SIZE 1024000//定义大小为1024000
+#define BLOCKNUM 1000//定义block数量为1000
+#define BLOCKSIZE 1024//定义block的大小为1024
+#define MAXOPENFILE 10//最大打开文件数10
+#define ROOTBLOCKNUM 2//根区块为2
 
 // utils
 // 这里是一些基础的函数，都是一些会重复利用的操作
+
 // 根据所给的参数对fcb进行初始化
 void fcb_init(fcb *new_fcb, const char* filename, unsigned short first, unsigned char attribute);
+
 // 根据所给的参数对一个打开文件项进行初始化
 void useropen_init(useropen *openfile, int dirno, int diroff, const char* dir);
+
 // 通过dfs对将已使用的fat块释放
 void fatFree(int id);
+
 // 得到一个空闲的fat块
 int getFreeFatid();
+
 // 得到一个空闲的打开文件表项
 int getFreeOpenlist();
+
 // 得到一个fat表的下一个fat表，如果没有则创建
 int getNextFat(int id);
+
 // 检查一个打开文件表下表是否合法
 int check_fd(int fd);
+
 // 把一个路径按'/'分割
 int spiltDir(char dirs[DIRLEN][DIRLEN], char *filename);
+
 // 把一个路径的最后一个目录从字符串中删去
 void popLastDir(char *dir);
+
 // 把一个路径的最后一个目录从字符串中分割出
 void splitLastDir(char *dir, char new_dir[2][DIRLEN]);
+
 // 得到某个长度在某个fat中的对应的盘块号和偏移量，用来记录一个打开文件项在其父目录对应fat的位置
 void getPos(int *id, int *offset, unsigned short first, int length);
+
 // 把路径规范化并检查
 int rewrite_dir(char *dir);
 
@@ -52,14 +71,16 @@ int rewrite_dir(char *dir);
 
 // 根据所给的参数对fcb进行初始化
 void fcb_init(fcb *new_fcb, const char* filename, unsigned short first, unsigned char attribute) {
-  strcpy(new_fcb->filename, filename); //这个strcpy(new_fcb->filename, filename)里面的内容是将filename的字符串复制给new_fcb,->意思是把filename的值给new_fcb
-									   //(pointer_name)->(variable_name) 
-									   //Operation: The -> operator in C or C++ gives the value held by variable_name to structure or union variable pointer_name 
-  new_fcb->first = first;   		   //将short类型的first文件起始盘块号赋值给new_fcb   first是文件起始盘块号
-  new_fcb->attribute = attribute;  	   //将char类型的attribute赋值给new_fcb attribute是文件属性字段，0为目录，1为文件
-  new_fcb->free = 0;				   //标记new_fcb没有被删除
-  if (attribute) new_fcb->length = 0;  //如果attribute==1，则将文件实际长度length=0赋值给new_fcb ，方便编辑文件
+  strcpy(new_fcb->filename, filename);  //这个strcpy(new_fcb->filename, filename)里面的内容是将filename的字符串复制给new_fcb,->意思是把filename的值给new_fcb，
+                                        //反正意思就是吧filename的字符串的值通过new_fcb复制给filename
+									                      //(pointer_name)->(variable_name) 
+									                      //Operation: The -> operator in C or C++ gives the value held by variable_name to structure or union variable pointer_name 
+  new_fcb->first = first;   		        //将short类型的first文件起始盘块号赋值给new_fcb   first是文件起始盘块号 
+  new_fcb->attribute = attribute;  	    //将char类型的attribute赋值给new_fcb attribute是文件属性字段，0为目录，1为文件
+  new_fcb->free = 0;				             //标记new_fcb没有被删除
+  if (attribute) new_fcb->length = 0;    //如果attribute==1，则将文件实际长度length=0赋值给new_fcb ，方便编辑文件
   else new_fcb->length = 2 * sizeof(fcb);//如果attribute ==0，则是文件夹，将文件实际长度=2*fcb的长度赋值给new_fcb
+  
   //对于文件夹fcb，其count永远等于其fcb的length，
   // 只有文件fcb的count会根据打开方式的不同和读写方式的不同而不同
 }
